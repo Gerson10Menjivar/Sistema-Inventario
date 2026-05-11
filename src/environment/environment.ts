cfec00e219f4d2459f9c4f75fab70be996661f09
Omar Menjivar <omarm@OmarMenjivar>
2026-05-10 22:43:18 -0600

    nuevos cambios

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://https://grupo6-backend-6.onrender.com/api/usuarios' // Asegúrate de que este sea el puerto donde corre tu Node.js
+  apiUrl: 'https://grupo6-backend-6.onrender.com/api' 
 };
